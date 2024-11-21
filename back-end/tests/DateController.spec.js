038016119ca8aa78e31657038f7e85f8093785ee
NikoZBK <nikozbk@gmail.com>
2024-11-21 16:27:00 -0500

    Add a test file for the date controller

---
+++ back-end/tests/DateController.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("DateController Tests", () => {
+  test("Add emotion log to a specific day", async () => {
+    // Test the addEmotionLog function
+    // Add an emotion log to a specific day
+    // Verify that the emotion log was added successfully
+  });
+
+  test("Edit a specific emotion log from a specific day", async () => {
+    // Test the editEmotionLog function
+    // Edit an emotion log from a specific day
+    // Verify that the emotion log was edited successfully
+  });
+});
