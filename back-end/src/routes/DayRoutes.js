50996bea3eb042d18037465e549997de8f27938b
Jesse Goldman <jigoldman@umass.edu>
2024-12-06 18:40:24 -0500

    updated dayroutes

--- back-end/src/routes/DayRoutes.js
+++ back-end/src/routes/DayRoutes.js
@@ -1,3 +1,44 @@
-class DayRoutes {}
+import express from "express";
+import DayController from "../controller/DayController.js";
+import { debugLog } from "../../config/debug.js";
+
+class DayRoutes {
+  constructor() {
+    this.router = express.Router();
+    this.initializeRoutes();
+  }
+
+  initializeRoutes() {
+    // Define the routes and connect them to controller methods
+
+    // Get all days
+    this.router.get("/days", async (req, res) => {
+      debugLog(`GET /days`);
+      await DayController.getAllData(req, res);
+    });
+
+    // Get a specific day
+    this.router.get("/days/:id", async (req, res) => {
+      debugLog(`GET /days/${req.params.id}`);
+      await DayController.getDay(req, res);
+    });
+
+    // Add a new day
+    this.router.post("/days/:id", async (req, res) => {
+      debugLog(`POST /days/:${req.params.id}`);
+      await DayController.addDay(req, res);
+    });
+
+    // Clear all days
+    this.router.delete("/days", async (req, res) => {
+      debugLog(`DELETE /days`);
+      await DayController.clearAllData(req, res);
+    });
+  }
+
+  getRouter() {
+    return this.router;
+  }
+}
 
 export default new DayRoutes().getRouter();
