0a9368cfb091cf511e2cef550283ac1af6990fdb
Nikolay Ostroukhov <nikozbk@gmail.com>
2024-12-18 17:01:17 -0500

    start from ground zero

--- back-end/src/middleware/middleware.js
+++
@@ -1,2 +0,0 @@
-export const successResponse = (data = null) => ({success: true, data});
-export const failedResponse =  (error = null) => ({success: false, error});
--- front-end/src/config/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  debug: true,
-  port: 3000,
-};
-
-export default config;
